f8d3f37c3e162e0d288cd6404a39d5b5af3d065c
idhyt <idhyt3r@gmail.com>
2024-05-11 10:39:08 +0800

    :construction_worker: Build

--- build.rs
+++ build.rs
@@ -2,7 +2,11 @@ fn main() {
     println!("cargo:rerun-if-changed=passh/passh.c");
 
     let mut builder: cc::Build = cc::Build::new();
+    #[cfg(debug_assertions)]
+    builder.flag_if_supported("-DDEBUG");
     builder
         .file("./passh/passh.c")
+        .warnings(false)
+        // .flag_if_supported("-DDEBUG")
         .compile("passh");
 }
