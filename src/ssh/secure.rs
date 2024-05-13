5ec99de2c95fcba72828df641ea975b19fd74fb1
idhyt <idhyt3r@gmail.com>
2024-05-13 14:02:39 +0800

    :sparkles: Add lazy...

--- src/ssh/secure.rs
+++ src/ssh/secure.rs
@@ -4,13 +4,20 @@ use chacha20poly1305::aead::generic_array::GenericArray;
 use chacha20poly1305::aead::{Aead, AeadCore, KeyInit, OsRng};
 use chacha20poly1305::ChaCha20Poly1305;
 
+lazy_static::lazy_static! {
+    static ref KEY: Option<String> = match std::env::var("ASKEY") {
+        Ok(key) => Some(key),
+        Err(_) => None,
+    };
+}
+
 fn generate_key(key: Option<&str>) -> Vec<u8> {
     if key.is_none() {
         return ChaCha20Poly1305::generate_key(&mut OsRng).to_vec();
     }
-
     let key = key.unwrap();
     let key = key.as_bytes().to_vec();
+
     // the key must 32 bytes
     if key.len() > 32 {
         return key[..32].to_vec();
@@ -41,27 +48,25 @@ fn chacha_decrypt(obsf: &[u8], key: &[u8]) -> String {
 }
 
 pub fn encrypt(data: &str) -> String {
-    // we get key from env
-    if let Ok(key) = std::env::var("ASKEY") {
-        log::debug!("we found key from env `ASKEY` and will use it to encrypt data.");
-        let key = generate_key(Some(&key));
-        let obsf = chacha_encrypt(data, &key);
-        return general_purpose::STANDARD_NO_PAD.encode(obsf);
+    if KEY.is_none() {
+        return data.to_string();
     }
-    data.to_string()
+    log::debug!("we found `ASKEY` and will encrypt.");
+    let key = generate_key(KEY.as_deref());
+    let obsf = chacha_encrypt(data, &key);
+    general_purpose::STANDARD_NO_PAD.encode(obsf)
 }
 
 pub fn decrypt(data: &str) -> String {
-    // we get key from env
-    if let Ok(key) = std::env::var("ASKEY") {
-        log::debug!("we found key from env `ASKEY` and will use it to decrypt data.");
-        let obsf = general_purpose::STANDARD_NO_PAD
-            .decode(data.as_bytes())
-            .unwrap();
-        let key = generate_key(Some(&key));
-        return chacha_decrypt(&obsf, &key);
+    if KEY.is_none() {
+        return data.to_string();
     }
-    data.to_string()
+    log::debug!("we found `ASKEY` and will decrypt.");
+    let obsf = general_purpose::STANDARD_NO_PAD
+        .decode(data.as_bytes())
+        .unwrap();
+    let key = generate_key(KEY.as_deref());
+    chacha_decrypt(&obsf, &key)
 }
 
 // tests
@@ -83,6 +88,8 @@ mod tests {
     #[test]
     fn test_secure() {
         // set env
+        let key = generate_key(None);
+        println!("secure key: {:?}", key);
         let key = "32 bit key must, if not, we will resize it.";
         std::env::set_var("ASKEY", key);
         let key = generate_key(Some(key));
