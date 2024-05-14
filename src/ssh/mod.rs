323066d26f985f18dce60b1e645f9edcde88ae99
idhyt <idhyt3r@gmail.com>
2024-05-14 12:20:50 +0800

    :recycle: Make mod is simple.

--- src/ssh/mod.rs
+++ src/ssh/mod.rs
@@ -2,98 +2,26 @@ mod bind;
 mod record;
 mod secure;
 
-use record::{Recorder, Remote};
+use record::Recorder;
 
 pub fn add(user: &str, password: &str, ip: &str, port: &u16, name: &Option<String>) {
-    log::debug!("add: {:?}, {:?}, {:?}, {:?}", user, ip, port, name);
-
     let mut recorder = Recorder::load();
-    log::debug!("records: {:#?}", recorder);
-
-    let indexs = recorder
-        .remotes
-        .iter()
-        .map(|v| v.index)
-        .collect::<Vec<u16>>();
-    let index = indexs.iter().max().unwrap_or(&0) + 1;
-    let remote = Remote {
-        index,
-        user: user.to_string(),
-        password: password.to_string(),
-        ip: ip.to_string(),
-        port: *port,
-        name: if let Some(name) = name {
-            Some(name.to_string())
-        } else {
-            Some(ip.to_string())
-        },
-    };
-    log::debug!("add remote: {}", remote);
-    recorder.remotes.push(remote);
-    recorder.save();
-    recorder.pprint();
-    log::debug!("add remote success");
+    let index = recorder.add(user, password, ip, port, name);
+    log::debug!("add remote success, index {}", index);
+    recorder.list();
 }
 
 pub fn list() {
-    let recorder = Recorder::load();
-    recorder.pprint();
+    Recorder::load().list();
 }
 
 pub fn remove(index: &u16) {
     let mut recorder = Recorder::load();
-    let index = *index;
-    let indexs = recorder
-        .remotes
-        .iter()
-        .map(|v| v.index)
-        .collect::<Vec<u16>>();
-    if !indexs.contains(&index) {
-        log::error!("the index {} not found", index);
-        return;
-    }
-    recorder.remotes.retain(|v| v.index != index);
-    recorder.save();
-    recorder.pprint();
-    log::debug!("remove remote success");
+    let index = recorder.delete(index);
+    log::debug!("remove remote success, index {}", index);
+    recorder.list();
 }
 
 pub fn login(index: &u16) {
-    let recorder = Recorder::load();
-    let index = *index;
-    let indexs = recorder
-        .remotes
-        .iter()
-        .map(|v| v.index)
-        .collect::<Vec<u16>>();
-    if !indexs.contains(&index) {
-        log::error!("the index {} not found", index);
-        return;
-    }
-    let remote = recorder.remotes.iter().find(|v| v.index == index).unwrap();
-    let cmd = format!("ssh {}@{} -p {}", remote.user, remote.ip, remote.port);
-    log::debug!("login remote: {}", cmd);
-
-    unsafe {
-        // passh -c 10 -p password ssh -p port user@ip
-        let argv = vec![
-            std::ffi::CString::new("passh").unwrap().into_raw(),
-            std::ffi::CString::new("-c").unwrap().into_raw(),
-            std::ffi::CString::new("10").unwrap().into_raw(),
-            std::ffi::CString::new("-p").unwrap().into_raw(),
-            std::ffi::CString::new(remote.password.clone())
-                .unwrap()
-                .into_raw(),
-            std::ffi::CString::new("ssh").unwrap().into_raw(),
-            std::ffi::CString::new("-p").unwrap().into_raw(),
-            std::ffi::CString::new(remote.port.to_string())
-                .unwrap()
-                .into_raw(),
-            std::ffi::CString::new(format!("{}@{}", remote.user, remote.ip))
-                .unwrap()
-                .into_raw(),
-        ];
-        let argc = argv.len() as i32;
-        bind::passh(argc, argv.as_ptr() as *mut *mut std::os::raw::c_char);
-    };
+    Recorder::load().login(index);
 }
--- src/ssh/record.rs
+++ src/ssh/record.rs
@@ -3,6 +3,7 @@ use prettytable::{color, Attr, Cell, Row, Table};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::path::PathBuf;
 
+use super::bind::passh;
 use super::secure::{decrypt, encrypt};
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -55,25 +56,7 @@ impl Recorder {
         UserDirs::new().unwrap().home_dir().join(".autossh.toml")
     }
 
-    pub fn load() -> Self {
-        let file = Self::file();
-        if !file.is_file() {
-            log::debug!("record file not found in {}", file.display());
-            return Self::default();
-        }
-
-        let content = std::fs::read_to_string(&file).expect("read record file failed");
-        let recorder: Recorder = toml::from_str(&content).expect("parse record file failed");
-        recorder
-    }
-
-    pub fn save(&self) {
-        let file = Self::file();
-        let content = toml::to_string(&self).unwrap();
-        std::fs::write(&file, content).unwrap();
-    }
-
-    pub fn pprint(&self) {
+    fn pprint(&self) {
         let mut table = Table::new();
         // table.add_row(Row::from(vec![
         //     "index".to_string(),
@@ -110,5 +93,110 @@ impl Recorder {
         }
         log::debug!("the remote list:\n{}", table);
         table.printstd();
+    }
+
+    fn save(&self) {
+        let file = Self::file();
+        let content = toml::to_string(&self).unwrap();
+        std::fs::write(&file, content).unwrap();
+    }
+
+    fn get(&self, index: &u16) -> Option<&Remote> {
+        let index = *index;
+        // let indexs = self
+        //     .remotes
+        //     .iter()
+        //     .map(|v| v.index)
+        //     .collect::<Vec<u16>>();
+        // if !indexs.contains(&index) {
+        //     log::error!("the index {} not found", index);
+        // }
+        for remote in self.remotes.iter() {
+            if remote.index == index {
+                return Some(remote);
+            }
+        }
+        log::error!("the index {} not found", index);
+        None
+    }
+
+    pub fn load() -> Self {
+        let file = Self::file();
+        if !file.is_file() {
+            log::debug!("record file not found in {}", file.display());
+            return Self::default();
+        }
+
+        let content = std::fs::read_to_string(&file).expect("read record file failed");
+        let recorder: Recorder = toml::from_str(&content).expect("parse record file failed");
+        recorder
+    }
+
+    pub fn list(&self) {
+        self.pprint();
+    }
+
+    pub fn add(
+        &mut self,
+        user: &str,
+        password: &str,
+        ip: &str,
+        port: &u16,
+        name: &Option<String>,
+    ) -> u16 {
+        let indexs = self.remotes.iter().map(|v| v.index).collect::<Vec<u16>>();
+        let index = indexs.iter().max().unwrap_or(&0) + 1;
+        let remote = Remote {
+            index,
+            user: user.to_string(),
+            password: password.to_string(),
+            ip: ip.to_string(),
+            port: *port,
+            name: if let Some(name) = name {
+                Some(name.to_string())
+            } else {
+                Some(ip.to_string())
+            },
+        };
+        log::debug!("add remote: {}", remote);
+        self.remotes.push(remote);
+        self.save();
+        index
+    }
+
+    pub fn delete(&mut self, index: &u16) -> u16 {
+        let index = *index;
+        self.remotes.retain(|v| v.index != index);
+        self.save();
+        index
+    }
+
+    pub fn login(&self, index: &u16) {
+        let remote = self.get(index).unwrap();
+        let cmd = format!("ssh {}@{} -p {}", remote.user, remote.ip, remote.port);
+        log::debug!("login remote: {}", cmd);
+
+        unsafe {
+            // passh -c 10 -p password ssh -p port user@ip
+            let argv = vec![
+                std::ffi::CString::new("passh").unwrap().into_raw(),
+                std::ffi::CString::new("-c").unwrap().into_raw(),
+                std::ffi::CString::new("10").unwrap().into_raw(),
+                std::ffi::CString::new("-p").unwrap().into_raw(),
+                std::ffi::CString::new(remote.password.clone())
+                    .unwrap()
+                    .into_raw(),
+                std::ffi::CString::new("ssh").unwrap().into_raw(),
+                std::ffi::CString::new("-p").unwrap().into_raw(),
+                std::ffi::CString::new(remote.port.to_string())
+                    .unwrap()
+                    .into_raw(),
+                std::ffi::CString::new(format!("{}@{}", remote.user, remote.ip))
+                    .unwrap()
+                    .into_raw(),
+            ];
+            let argc = argv.len() as i32;
+            passh(argc, argv.as_ptr() as *mut *mut std::os::raw::c_char);
+        };
     }
 }
