d4d1186dd1a5acca34be2b149076467bb6c09da3
idhyt <idhyt3r@gmail.com>
2024-11-04 10:24:07 +0800

    begin to dev branch

--- src/ssh/mod.rs
+++ src/ssh/mod.rs
@@ -1,4 +1,3 @@
-mod bind;
 mod secure;
 pub mod server;
 
--- src/ssh/server.rs
+++ src/ssh/server.rs
@@ -1,8 +1,6 @@
 use prettytable::{Cell, Row, Table};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[cfg(target_family = "unix")]
-use super::bind::passh;
 use super::secure::{decrypt, encrypt};
 
 #[derive(Debug, Default, Serialize, Deserialize, Clone)]
@@ -47,60 +45,8 @@ impl std::fmt::Display for Remote {
 }
 
 impl Remote {
-    #[cfg(target_family = "unix")]
     pub fn login(&self) {
         log::debug!("login at: {}", self);
-
-        unsafe {
-            // passh -c 10 -p password ssh -p port user@ip
-            let argv = vec![
-                std::ffi::CString::new("passh").unwrap().into_raw(),
-                std::ffi::CString::new("-c").unwrap().into_raw(),
-                std::ffi::CString::new("10").unwrap().into_raw(),
-                std::ffi::CString::new("-p").unwrap().into_raw(),
-                std::ffi::CString::new(self.password.clone())
-                    .unwrap()
-                    .into_raw(),
-                std::ffi::CString::new("ssh").unwrap().into_raw(),
-                std::ffi::CString::new("-p").unwrap().into_raw(),
-                std::ffi::CString::new(self.port.to_string())
-                    .unwrap()
-                    .into_raw(),
-                std::ffi::CString::new(format!("{}@{}", self.user, self.ip))
-                    .unwrap()
-                    .into_raw(),
-            ];
-            let argc = argv.len() as i32;
-            passh(argc, argv.as_ptr() as *mut *mut std::os::raw::c_char);
-        };
-    }
-
-    #[cfg(target_os = "windows")]
-    pub fn login(&self) {
-        let putty_path = {
-            let mut exe_path = std::env::current_exe().unwrap();
-            exe_path.pop();
-            exe_path.push("putty.exe");
-            exe_path
-        };
-        if !putty_path.exists() {
-            log::error!("`putty.exe` not found, you can download it from `https://www.chiark.greenend.org.uk/~sgtatham/putty/` and put it to {}.", putty_path.display());
-            return;
-        }
-        // putty.exe -ssh user@ip -P port -pw password
-        let cmd = format!(
-            "{} -ssh {}@{} -P {} -pw {}",
-            putty_path.display(),
-            self.user,
-            self.ip,
-            self.port,
-            self.password
-        );
-        log::debug!("login at: {}", self);
-        std::process::Command::new("cmd")
-            .args(&["/C", &cmd])
-            .spawn()
-            .unwrap();
     }
 }
 
