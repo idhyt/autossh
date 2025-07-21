69c8fd2e295554a4ab7e79cc56cc7afe5c863934
idhyt <idhyt3r@gmail.com>
2025-07-21 19:17:54 +0800

    👷: Build to compatible old version name

--- cli/build.rs
+++ cli/build.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 const VERSION: &str = "0.4.0";
@@ -24,6 +24,19 @@ fn get_version() -> String {
     format!("{} ({} {})", VERSION, commit_hash, commit_date)
 }
 
+fn patch_file(file: &Path, key: &str, value: &str) {
+    let content = std::fs::read_to_string(file).unwrap();
+    let mut lines: Vec<&str> = content.lines().map(|s| s).collect();
+    for i in 0..lines.len() {
+        if lines[i].contains(key) {
+            lines[i] = value;
+            break;
+        }
+    }
+    std::fs::write(file, lines.join("\n") + "\n").unwrap();
+    println!("success patch consts {key} to {value}");
+}
+
 fn main() {
     // if cross compiling, unsupport get version because of no git
     if std::env::var("CROSS_RUNNER").is_ok() {
@@ -39,14 +52,9 @@ fn main() {
     assert!(cli_path.is_file());
 
     let version_code = format!("    version = \"{}\",", version);
-    let content = std::fs::read_to_string(&cli_path).unwrap();
-    let mut lines: Vec<&str> = content.lines().map(|s| s).collect();
-    for i in 0..lines.len() {
-        if lines[i].contains("version") {
-            lines[i] = &version_code;
-            break;
-        }
-    }
-    std::fs::write(cli_path, lines.join("\n") + "\n").unwrap();
-    println!("success patch consts version to {version}");
+    patch_file(&cli_path, "version", &version_code);
+
+    let package_toml = build_dir.join("Cargo.toml");
+    assert!(package_toml.is_file());
+    patch_file(&package_toml, "name", "name = \"atsh\"");
 }
